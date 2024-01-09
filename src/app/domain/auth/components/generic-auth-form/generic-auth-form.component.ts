056ae8df13a90bcbdf4299b322e58f64853725a8
Jorge <jorgevitorp9@gmail.com>
2024-01-09 01:25:03 -0300

    ✨ feat(generic-auth-form): showing errors in template with show-errors-input.directive

--- src/app/domain/auth/components/generic-auth-form/generic-auth-form.component.ts
+++ src/app/domain/auth/components/generic-auth-form/generic-auth-form.component.ts
@@ -1,13 +1,19 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ShowInputErrorsDirective } from '../../../../directives/show-input-errors/show-input-errors.directive';
 import { FieldOptions } from '../../interfaces/signup';
 
 @Component({
   selector: 'ta-generic-auth-form',
   standalone: true,
-  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule],
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    ShowInputErrorsDirective,
+  ],
   templateUrl: './generic-auth-form.component.html',
   styleUrl: './generic-auth-form.component.scss',
 })
