0f23cdbca8c103a2238153c4f5a200b469ef95b7
Jorge <jorgevitorp9@gmail.com>
2024-01-09 01:27:35 -0300

    ✨ feat(encrypt-storage.service): setting encrypted values in local storage with encrypt-storage and getting decrypted items in localstorage with encrypt-storage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { EncryptStorageService } from './services/utils/encrypt-storage.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
      provideRouter(routes),
      provideAnimations(), 
+    EncryptStorageService,
     ],
 };
---
+++ src/app/services/utils/encrypt-storage.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { EncryptStorage } from 'encrypt-storage';
+import { environment } from '../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EncryptStorageService {
+  private encryptStorage = new EncryptStorage(environment.SECRET_KEY);
+
+  setItem(key: string, value: string) {
+    this.encryptStorage.setItem(key, value);
+  }
+
+  getItem(key: string) {
+    const item = this.encryptStorage.getItem(key);
+    return item;
+  }
+}
