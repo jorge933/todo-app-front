8400c5b4f00cabb093260f7d0189b6344a74d64f
Jorge <jorgevitorp9@gmail.com>
2024-01-10 15:01:27 -0300

    ✨ feat: interceptor add token in request

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,12 +6,15 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { EncryptStorageService } from './services/utils/encrypt-storage.service';
 import { requestErrorInterceptor } from './interceptors/request-error/request-error.interceptor';
+import { addTokenInRequestInterceptor } from './interceptors/add-token-in-request/add-token-in-request.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(withInterceptors([requestErrorInterceptor])),
+    provideHttpClient(
+      withInterceptors([requestErrorInterceptor, addTokenInRequestInterceptor])
+    ),
     EncryptStorageService,
   ],
 };
---
+++ src/app/interceptors/add-token-in-request/add-token-in-request.interceptor.ts
@@ -0,0 +1,24 @@
+import { HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { EncryptStorageService } from '../../services/utils/encrypt-storage.service';
+
+export const addTokenInRequestInterceptor: HttpInterceptorFn = (
+  request,
+  next
+) => {
+  const includesAuthInRoute = request.url.includes('auth');
+  if (includesAuthInRoute) return next(request);
+
+  const encryptStorageService = inject(EncryptStorageService);
+  const token = encryptStorageService.getItem('token');
+
+  const headers = new HttpHeaders({
+    Authorization: 'Bearer ' + token,
+  });
+
+  const newRequest = request.clone({
+    headers,
+  });
+
+  return next(newRequest);
+};
