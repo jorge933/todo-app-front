c23c99a9abcb337ed9df107ac48c1d79d512d5e8
Jorge <jorgevitorp9@gmail.com>
2024-01-09 22:50:36 -0300

    ✨ feat: request-error.interceptor in folder on interceptors folder

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { EncryptStorageService } from './services/utils/encrypt-storage.service';
-import { requestInterceptor } from './interceptors/request.interceptor';
+import { requestInterceptor } from './interceptors/request-error/request-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- src/app/interceptors/request-error/request-error.interceptor.ts
+++ src/app/interceptors/request-error/request-error.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { catchError } from 'rxjs';
 import { inject } from '@angular/core';
-import { HttpErrorTypeService } from '../services/http-error-type/http-error-type.service';
+import { HttpErrorTypeService } from '../../services/http-error-type/http-error-type.service';
 
 export const requestInterceptor: HttpInterceptorFn = (req, next) => {
   const httpErrorTypeService = inject(HttpErrorTypeService);
