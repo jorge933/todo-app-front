b3adb437d835f08739bf77e70135222c1a10c29e
Jorge <jorgevitorp9@gmail.com>
2024-01-10 23:55:27 -0300

    ✨ feat(tasks.page): create task form in mat-expansion-panel

--- src/app/domain/tasks/pages/tasks/tasks.page.ts
+++ src/app/domain/tasks/pages/tasks/tasks.page.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatInputModule } from '@angular/material/input';
 import { ControlErrorContainerDirective } from '../../../../directives/control-errors-container/control-errors-container.directive';
 import { ShowInputErrorsDirective } from '../../../../directives/show-input-errors/show-input-errors.directive';
@@ -18,6 +19,7 @@ import { TasksService } from '../../services/tasks/tasks.service';
     ReactiveFormsModule,
     ShowInputErrorsDirective,
     ControlErrorContainerDirective,
+    MatExpansionModule,
   ],
   providers: [TasksService],
   templateUrl: './tasks.page.html',
