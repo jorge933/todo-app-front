d211635e17a5c1aada1e04c188275ea85e4f2907
Jorge <jorgevitorp9@gmail.com>
2024-01-16 17:42:14 -0300

    ✨ feat(domain): tasks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { authRoutes } from './domain/auth/auth.routes';
 import { authRoutesGuard } from './guards/auth-routes.guard';
+import { tasksRoutes } from './domain/tasks/tasks.routes';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -12,5 +14,10 @@ export const routes: Routes = [
     path: 'auth',
     children: authRoutes,
     canActivate: [authRoutesGuard],
+  },
+  {
+    path: 'tasks',
+    children: tasksRoutes,
+    canActivate: [authGuard],
   },
 ];
---
+++ src/app/domain/tasks/pages/tasks/tasks.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [],
+  templateUrl: './tasks.page.html',
+  styleUrl: './tasks.page.scss',
+})
+export default class TasksPage {}
