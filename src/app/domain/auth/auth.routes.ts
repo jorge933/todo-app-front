044104ab6ddf9921f9198a48074253a52cff0418
Jorge <jorgevitorp9@gmail.com>
2024-01-17 10:52:07 -0300

    ✨ feat(auth.routes.ts): title for pages

--- src/app/domain/auth/auth.routes.ts
+++ src/app/domain/auth/auth.routes.ts
@@ -4,10 +4,12 @@ export const authRoutes: Routes = [
   {
     path: 'signup',
     loadComponent: () => import('./pages/signup/signup.page'),
+    title: 'Signup | TodoApp',
   },
 
   {
     path: 'login',
     loadComponent: () => import('./pages/login/login.page'),
+    title: 'Login | TodoApp',
   },
 ];
